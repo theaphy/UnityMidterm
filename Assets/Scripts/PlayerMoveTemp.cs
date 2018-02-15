30312f289ae9d371a82ab611c5508ec3a92d17b9
alecheadrick <35284976+alecheadrick@users.noreply.github.com>
2018-02-14 21:08:28 -0500

    fixed import problem

--- Assets/Scripts/PlayerMoveTemp.cs
+++ Assets/Scripts/PlayerMoveTemp.cs
@@ -2,46 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMoveTemp : MonoBehaviour {
+public class PlayerMoveTemp : MonoBehaviour
+{
 	#region Variables
 	public float moveSpeed;
 	private Vector3 target;
 	#endregion
-	
+
 	#region Methods
 
-	void Start () {
+	void Start()
+	{
 		target = transform.position;
 	}
-	
-//	void Update () {
-//		if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) {
-//			transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
-//		}
-//		if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
-//		{
-//			transform.Translate(new Vector3(0f,Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-//		}
-//	}
 
-	void Update () {
-<<<<<<< HEAD
-		if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) {
-			transform.Translate(new Vector3(0f, Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f));
+	//	void Update () {
+	//		if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) {
+	//			transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+	//		}
+	//		if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+	//		{
+	//			transform.Translate(new Vector3(0f,Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+	//		}
+	//	}
+
+	void Update()
+	{
+			if (Input.GetMouseButtonDown(0))
+			{
+				target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				target.z = transform.position.z;
+			}
+			transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
 		}
-		if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
-		{
-			transform.Translate(new Vector3(0f,Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-=======
-		if (Input.GetMouseButtonDown(0)) {
-			target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			target.z = transform.position.z;
->>>>>>> 3736a85b9d8831b92449409f017e5b62163d4f5f
-		}
-		transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
-	}    
-	
+	}
 	#endregion
-
-}
-
