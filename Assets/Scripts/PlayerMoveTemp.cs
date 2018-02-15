bc3e77f7137e8892363a83854c69082254903977
theaphy <theheaphy@gmail.com>
2018-02-14 20:37:14 -0500

    click to move player, not tap yet

--- Assets/Scripts/PlayerMoveTemp.cs
+++ Assets/Scripts/PlayerMoveTemp.cs
@@ -5,19 +5,34 @@ using UnityEngine;
 public class PlayerMoveTemp : MonoBehaviour {
 	#region Variables
 	public float moveSpeed;
+	private Vector3 target;
 	#endregion
 	
 	#region Methods
-	
-	void Update () {
-		if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) {
-			transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
-		}
-		if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
-		{
-			transform.Translate(new Vector3(0f,Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-		}
+
+	void Start () {
+		target = transform.position;
 	}
 	
+//	void Update () {
+//		if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) {
+//			transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+//		}
+//		if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+//		{
+//			transform.Translate(new Vector3(0f,Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+//		}
+//	}
+
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			target.z = transform.position.z;
+		}
+		transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+	}    
+	
 	#endregion
+
 }
+
